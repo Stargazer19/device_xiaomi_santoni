b3894673d4f9095af1aaf093a603bdedabc8b773
Stargazer19 <pviraj19@gmail.com>
2020-03-06 17:14:30 +0530

    santoni: Update lunch choices

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -2,4 +2,7 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/xtended_santoni.mk
 
 COMMON_LUNCH_CHOICES := \
-    xtended_santoni-userdebug
+    xtended_santoni-userdebug \
+    xtended_santoni-user \
+    xtended_santoni-eng
+
