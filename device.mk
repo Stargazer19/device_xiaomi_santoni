af3ed958a03f73a3be40eec56ec9e4399d6b9af3
intervigilium <intervigil@gmail.com>
2020-03-16 16:05:15 +0530

    santoni: Add basic USB HAL that reports no status change

--- device.mk
+++ device.mk
@@ -465,7 +465,7 @@ PRODUCT_COPY_FILES += \
 
 # USB HAL
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service.basic
+    android.hardware.usb@1.0-service.msm8937
 
 # Vibrator
 PRODUCT_PACKAGES += \
